b90a0f5b62629f8f16e3b395be23ee0dacc48aad
HU-03 <HU-03@HU-03-PC>
2020-05-07 14:27:06 +0900

    05.07 접근제한자

--- src/com/human/abstract_interface/Test.java
+++ src/com/human/abstract_interface/Test.java
@@ -3,17 +3,31 @@ package com.human.abstract_interface;
 //public 모든 위치에서 접근가능
 //private 같은 클래스 내부에서 접근가능
 //default 같은 패키지에서 접근가능
-//protected 상속받은 자식에서만 접근가능
-
+//protected 같은 패키지이거나 다른패키지면 상속받은 자식에서만 접근가능
+class MyClass2 extends com.human.ex1.MyClass{
+	public void test() {
+		i1=1;
+//		i2=1;
+//		i3=1;
+		i4=1;
+		
+	}
+}
 public class Test {
 
 	public static void main(String[] args) {
-		MyClass mc=new MyClass();
+		com.human.abstract_interface.MyClass mc=new com.human.abstract_interface.MyClass();
 		mc.i1=1;
 		// 접근 제한자 private mc.i2=1;
 		mc.i3=1;
 		mc.i4=1;
 		
+		com.human.ex1.MyClass mc2=new com.human.ex1.MyClass();
+		mc2.i1=1;
+//		mc2.i2=1;
+//		mc2.i3=1;
+//		mc2.i4=1;
+//		
 	}
 
 }
